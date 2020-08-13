03637a12d9f4482f67222136be164b75144486de
Gantushig Bayasgalan <gantushig21@gmail.com>
2020-08-12 20:22:45 -0500

    target deleted

--- target/SpringMVC-Ecommerce/resources/js/constants.js
+++
@@ -1,4 +0,0 @@
-export const contextRoot = "/" + window.location.pathname.split( '/' )[1];
-export const token = $("meta[name='_csrf_token']").attr("content");
-export const parameterName = $("meta[name='_csrf_parameterName']").attr("content");
-
