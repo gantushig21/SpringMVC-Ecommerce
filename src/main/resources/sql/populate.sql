589b1af313848fac41e0729d1a4b950a78ee3c75
Gantushig Bayasgalan <gantushig21@gmail.com>
2020-08-12 20:27:35 -0500

    update

--- src/main/resources/sql/populate.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO credentials(username,password,enabled) VALUES ('guest','$2a$10$0.ESlGysrPaiW5HaapKwoehzWt5AibgbPPOvMhDv8D6H26QQ/CwhS', TRUE);
-INSERT INTO credentials(username,password,enabled) VALUES ('admin','$2a$10$S/wlXEo/APzf.Sn1cO2p4.V12EJmaw.uzrHelMvkpuahjmHWnSafe', TRUE);
-
-INSERT INTO authority (id,username, authority) VALUES (1,'guest', 'ROLE_USER');
-INSERT INTO authority (id,username, authority) VALUES (2,'admin', 'ROLE_ADMIN');
-INSERT INTO authority (id,username, authority) VALUES (3,'admin', 'ROLE_USER');
-
-
